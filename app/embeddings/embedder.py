cb805d38bc7010c4c90d7fd28199ea20fc4509bd
Bharatmish <bm4957@srmist.edu.in>
2025-08-05 13:28:45 +0530

    update memory embedder

--- app/embeddings/embedder.py
+++ app/embeddings/embedder.py
@@ -1,7 +1,12 @@
 # app/embeddings/embedder.py
 from sentence_transformers import SentenceTransformer
+from functools import lru_cache
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+@lru_cache(maxsize=1)
+def get_model():
+    return SentenceTransformer("all-MiniLM-L6-v2")
 
 def embed_text(text: str):
+    model = get_model()
     return model.encode(text)
+
